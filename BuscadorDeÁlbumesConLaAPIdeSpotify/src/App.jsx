6796a11ba1df61db9eeaf8651a0c5b5833ae32f0
omarelfounti <elfountiomar@gmail.com>
2025-03-09 19:19:46 +0100

    frontend creado

---
+++ BuscadorDeÁlbumesConLaAPIdeSpotify/src/App.jsx
@@ -0,0 +1,83 @@
+import "./App.css";
+import { FormControl, InputGroup, Container, Button } from "react-bootstrap";
+import { useState, useEffect } from "react";
+
+const clientId = import.meta.env.VITE_CLIENT_ID;
+const clientSecret = import.meta.env.VITE_CLIENT_SECRET;
+
+function App() {
+  const [searchInput, setSearchInput] = useState("");
+  const [accessToken, setAccessToken] = useState("");
+
+  useEffect(() => {
+    let authParams = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body:
+        "grant_type=client_credentials&client_id=" +
+        clientId +
+        "&client_secret=" +
+        clientSecret,
+    };
+    fetch("https://accounts.spotify.com/api/token", authParams)
+      .then((result) => result.json())
+      .then((data) => {
+        setAccessToken(data.access_token);
+      });
+  }, []);
+
+  async function search() {
+    let artistParams = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + accessToken,
+      },
+    };
+
+    
+    const artistID = await fetch(
+      "https://api.spotify.com/v1/search?q=" + searchInput + "&type=artist",
+      artistParams
+    )
+      .then((result) => result.json())
+      .then((data) => {
+        return data.artists.items[0].id;
+      });
+  }
+
+  return (
+    <>
+      <Container>
+        <InputGroup>
+          <FormControl
+            placeholder="Search For Artist"
+            type="input"
+            aria-label="Search for an Artist"
+            onKeyDown={(event) => {
+              if (event.key === "Enter") {
+                search();
+              }
+            }}
+            onChange={(event) => setSearchInput(event.target.value)}
+            style={{
+              width: "300px",
+              height: "35px",
+              borderWidth: "0px",
+              borderStyle: "solid",
+              borderRadius: "5px",
+              marginRight: "10px",
+              paddingLeft: "10px",
+            }}
+          />
+
+          <Button onClick={search}>Search</Button>
+        </InputGroup>
+      </Container>
+    </>
+  );
+}
+
+export default App;
